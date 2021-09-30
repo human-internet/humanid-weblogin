5d04d5ba9e1cffb4ae1271f7344ac319cddabfb9
Saggaf <saggaf.arsyad@gmail.com>
2021-09-30 20:17:56 +0700

    [ADDED] add source parameter to determine source of login request

--- src/application/core/MY_Controller.php
+++ src/application/core/MY_Controller.php
@@ -1,11 +1,17 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require_once APPPATH.'core/AppMaster.php';
-class MY_Controller extends AppMaster {
+require_once APPPATH . 'core/AppMaster.php';
 
-    var $lg, $pc;
-    
+/**
+ * @property CI_Input $input
+ * @property CI_User_agent $agent
+ */
+class MY_Controller extends AppMaster
+{
+
+    var $lg, $pc, $source;
+
     function __construct()
     {
         parent::__construct(array(
@@ -14,89 +20,88 @@ class MY_Controller extends AppMaster {
         $this->load->library('session');
 
         $lang = $this->input->get('lang', TRUE);
-		$this->lg = $this->init_language($lang);
+        $this->lg = $this->init_language($lang);
         $this->data['lang'] = $this->lg;
 
         $prio = $this->input->get('priority_country', TRUE);
-		$this->pc = $this->init_priority_country($prio);
+        $this->pc = $this->init_priority_country($prio);
         $this->data['pc'] = $this->pc;
-        
+
+        $this->source = $this->input->get('s', TRUE);
+
         $this->init_logs();
     }
 
-    public function init_language($lang='')
+    public function init_language($lang = '')
     {
         $lang_session = $this->session->userdata('humanid_language');
-        if($lang_session && empty($lang)){
+        if ($lang_session && empty($lang)) {
             $lang = $lang_session;
-        }
-        else{
+        } else {
             $this->session->set_userdata(array('humanid_language' => $lang));
         }
 
-        $path = empty(getenv('LANG_PATH')) ? str_replace('system','language',BASEPATH) : getenv('LANG_PATH');
-        $filename = $path.$lang.'.json';
-        if(!file_exists($filename)){
+        $path = empty(getenv('LANG_PATH')) ? str_replace('system', 'language', BASEPATH) : getenv('LANG_PATH');
+        $filename = $path . $lang . '.json';
+        if (!file_exists($filename)) {
             $lang = getenv('LANG_CODE');
-            $filename = $path.$lang.'.json';
+            $filename = $path . $lang . '.json';
             $this->session->set_userdata(array('humanid_language' => $lang));
         }
 
         $string = file_get_contents($filename);
         $row = json_decode($string, FALSE);
         $row->id = $lang;
-        
+
         return $row;
     }
 
-    private function init_priority_country($code='')
+    private function init_priority_country($code = '')
     {
         $priority_country = $this->session->userdata('humanid_priority_country');
-        if($priority_country && empty($code)){
+        if ($priority_country && empty($code)) {
             $code = $priority_country;
-        }
-        else{
+        } else {
             $this->session->set_userdata(array('humanid_priority_country' => $code));
         }
         $_code = 'us';
         $row = array(
             'code' => $_code,
-            'code_js' => '["'.$_code.'"]'
+            'code_js' => '["' . $_code . '"]'
         );
-        if($code){
+        if ($code) {
             $_code = array();
             $_code_js = array();
             $codes = explode(',', $code);
-            foreach($codes as $c){
-                if($c){
+            foreach ($codes as $c) {
+                if ($c) {
                     $c = strtolower($c);
                     $_code[] = $c;
-                    $_code_js[] = '"'.$c.'"';
+                    $_code_js[] = '"' . $c . '"';
                 }
             }
-            if(!empty($_code)){
+            if (!empty($_code)) {
                 $row['code'] = implode(',', $_code);
-                $row['code_js'] = '['.implode(',', $_code_js).']';
+                $row['code_js'] = '[' . implode(',', $_code_js) . ']';
             }
         }
 
-        return (object) $row;
+        return (object)$row;
     }
 
-    public function init_logs($res=array())
+    public function init_logs($res = array())
     {
-        if(getenv('LOG_ACTIVITY'))
-        {
+        if (getenv('LOG_ACTIVITY')) {
             $token = $this->input->get('t', TRUE);
-            if(!$token){
+            if (!$token) {
                 $token = 'no-token-jwt';
             }
             $appId = $this->input->get('a', TRUE);
-            if(!$appId){
+            if (!$appId) {
                 $appId = 'no-appId';
             }
             $lang = $this->input->get('lang', TRUE);
-            if(!$lang){
+            if (!$lang) {
                 $lang = 'no-lang';
             }
             $error = (isset($res['error'])) ? $res['error'] : $this->session->flashdata('error_message');
@@ -104,32 +109,31 @@ class MY_Controller extends AppMaster {
             $method = ($_POST) ? 'post' : 'get';
             $this->load->library('user_agent');
             $data = array(
-                'Token'     => $token,
-                'AppId'     => $appId,
-                'Language'  => $lang,
+                'Token' => $token,
+                'AppId' => $appId,
+                'Language' => $lang,
                 'IPAddress' => $this->input->ip_address(),
-                'Browser'   => $this->agent->browser(),
-                'Version'   => $this->agent->version(),
-                'Mobile'    => $this->agent->mobile(),
-                'Url'       => $url,
-                'method'	=> $method,
-                'Error'     => $error,
-                'Created'   => date('Y-m-d H:i:s'),
+                'Browser' => $this->agent->browser(),
+                'Version' => $this->agent->version(),
+                'Mobile' => $this->agent->mobile(),
+                'Url' => $url,
+                'method' => $method,
+                'Error' => $error,
+                'Created' => date('Y-m-d H:i:s'),
             );
 
             $path = getenv('LOG_PATH');
-            $path = (empty($path)) ? str_replace('system','logs',BASEPATH) : $path;
-            $file = $path . date('Ymd').'.csv';
+            $path = (empty($path)) ? str_replace('system', 'logs', BASEPATH) : $path;
+            $file = $path . date('Ymd') . '.csv';
             $csv = '';
-            if(!file_exists($file))
-            {
+            if (!file_exists($file)) {
                 $first = true;
                 foreach ($data as $k => $v) {
-                    if(!$first){
+                    if (!$first) {
                         $csv .= ",";
                     }
                     $csv .= $k;
-                    if($first){
+                    if ($first) {
                         $first = false;
                     }
                 }
@@ -137,24 +141,24 @@ class MY_Controller extends AppMaster {
             }
             $first = true;
             foreach ($data as $k => $v) {
-                if(!$first){
+                if (!$first) {
                     $csv .= ",";
                 }
                 $csv .= $this->csv_field($v);
-                if($first){
+                if ($first) {
                     $first = false;
                 }
             }
             $csv .= "\n";
-            $csv_handler = fopen($file,'a+');
-            fwrite($csv_handler,$csv);
+            $csv_handler = fopen($file, 'a+');
+            fwrite($csv_handler, $csv);
             fclose($csv_handler);
         }
     }
 
-    private function csv_field($string) 
+    private function csv_field($string)
     {
-        if(strpos($string, ',') !== false || strpos($string, '"') !== false || strpos($string, "\n") !== false) {
+        if (strpos($string, ',') !== false || strpos($string, '"') !== false || strpos($string, "\n") !== false) {
             $string = '"' . str_replace('"', '""', $string) . '"';
         }
         return $string;
--- src/application/libraries/Humanid.php
+++ src/application/libraries/Humanid.php
@@ -1,5 +1,8 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
+/**
+ * @property CI_Controller $ci
+ */
 class Humanid
 {
     var $url;
@@ -7,11 +10,11 @@ class Humanid
     var $client_secret;
     var $server_id;
     var $server_secret;
-    
+
     function __construct()
     {
         $this->ci =& get_instance();
-        
+
         $this->url = getenv('HUMANID_URL');
         $this->client_id = getenv('HUMANID_CLIENT_ID');
         $this->client_secret = getenv('HUMANID_CLIENT_SECRET');
@@ -20,24 +23,23 @@ class Humanid
 
     }
 
-    public function app_info($appId)
+    public function app_info($appId, $source = "w")
     {
         $res = array(
             'send' => FALSE,
             'result' => 'No response!'
         );
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url .'web-login/apps/'.$appId);
+        curl_setopt($ch, CURLOPT_URL, $this->url . 'web-login/apps/' . $appId . '?s=' . $source);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','client-id:'. $this->client_id,'client-secret:'.$this->client_secret));
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'client-id:' . $this->client_id, 'client-secret:' . $this->client_secret));
         $result = curl_exec($ch);
-        if($result===false){
+        if ($result === false) {
             $res['result'] = 'Curl failed: ' . curl_error($ch);
-        }
-        else {
+        } else {
             $res['send'] = TRUE;
             $res['result'] = json_decode($result, true);
         }
@@ -46,7 +48,7 @@ class Humanid
         return $res;
     }
 
-    public function session($clientId,$clientSecret,$debug=false)
+    public function session($clientId, $clientSecret, $debug = false)
     {
         $res = array(
             'send' => FALSE,
@@ -59,23 +61,22 @@ class Humanid
         );
         $data_json = json_encode($fields);
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url .'web-login/sessions');
+        curl_setopt($ch, CURLOPT_URL, $this->url . 'web-login/sessions');
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json),'client-id:'. $this->client_id,'client-secret:'.$this->client_secret));
-        if($debug){
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data_json), 'client-id:' . $this->client_id, 'client-secret:' . $this->client_secret));
+        if ($debug) {
             $res['fields'] = json_encode($fields);
         }
         $result = curl_exec($ch);
-        if($result===false){
+        if ($result === false) {
             $res['result'] = 'Curl failed: ' . curl_error($ch);
-        }
-        else {
+        } else {
             $res['send'] = TRUE;
             $res['result'] = json_decode($result, true);
         }
@@ -84,7 +85,7 @@ class Humanid
         return $res;
     }
 
-    public function request_otp($countryCode,$phone,$token,$lang='en_US',$debug=false) 
+    public function request_otp($countryCode, $phone, $token, $source = "w", $lang = 'en_US', $debug = false)
     {
         $res = array(
             'send' => FALSE,
@@ -92,28 +93,27 @@ class Humanid
         );
 
         $fields = array(
-            'countryCode='. $countryCode,
-            'phone='. $phone,
-            'token='. $token
+            'countryCode=' . $countryCode,
+            'phone=' . $phone,
+            'token=' . $token
         );
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url .'web-login/users/request-otp?lang='.$lang);
+        curl_setopt($ch, CURLOPT_URL, $this->url . 'web-login/users/request-otp?lang=' . $lang . '&s=' . $source);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $fields));
-        curl_setopt($ch, CURLOPT_HEADER, FALSE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded','client-id:'. $this->client_id,'client-secret:'.$this->client_secret));
-        if($debug){
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded', 'client-id:' . $this->client_id, 'client-secret:' . $this->client_secret));
+        if ($debug) {
             $res['fields'] = json_encode($fields);
         }
         $result = curl_exec($ch);
-        if($result===false){
+        if ($result === false) {
             $res['result'] = 'Curl failed: ' . curl_error($ch);
-        }
-        else {
+        } else {
             $res['send'] = TRUE;
             $res['result'] = json_decode($result, true);
         }
@@ -122,7 +122,7 @@ class Humanid
         return $res;
     }
 
-    public function verify_otp($countryCode,$phone,$verificationCode,$token,$debug=false) 
+    public function verify_otp($countryCode, $phone, $verificationCode, $token, $source = "w", $debug = false)
     {
         $res = array(
             'send' => FALSE,
@@ -130,31 +130,30 @@ class Humanid
         );
 
         $fields = array(
-            'countryCode='. $countryCode,
-            'phone='. $phone,
+            'countryCode=' . $countryCode,
+            'phone=' . $phone,
             'deviceId=device-id',
-            'verificationCode='. $verificationCode,
+            'verificationCode=' . $verificationCode,
             'notifId=NONE',
-            'token='.$token
+            'token=' . $token
         );
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url .'web-login/users/login');
+        curl_setopt($ch, CURLOPT_URL, $this->url . 'web-login/users/login?s=' . $source);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $fields));
-        curl_setopt($ch, CURLOPT_HEADER, FALSE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded','client-id:'. $this->client_id,'client-secret:'.$this->client_secret));
-        if($debug){
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded', 'client-id:' . $this->client_id, 'client-secret:' . $this->client_secret));
+        if ($debug) {
             $res['fields'] = json_encode($fields);
         }
         $result = curl_exec($ch);
-        if($result===false){
+        if ($result === false) {
             $res['result'] = 'Curl failed: ' . curl_error($ch);
-        }
-        else {
+        } else {
             $res['send'] = TRUE;
             $res['result'] = json_decode($result, true);
         }
@@ -163,7 +162,7 @@ class Humanid
         return $res;
     }
 
-    public function exchange($token,$debug=false) 
+    public function exchange($token, $debug = false)
     {
         $res = array(
             'send' => FALSE,
@@ -175,23 +174,22 @@ class Humanid
         );
         $data_json = json_encode($fields);
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url .'server/users/exchange');
+        curl_setopt($ch, CURLOPT_URL, $this->url . 'server/users/exchange');
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json),'client-id:'. $this->server_id,'client-secret:'.$this->server_secret));
-        if($debug){
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data_json), 'client-id:' . $this->server_id, 'client-secret:' . $this->server_secret));
+        if ($debug) {
             $res['fields'] = json_encode($fields);
         }
         $result = curl_exec($ch);
-        if($result===false){
+        if ($result === false) {
             $res['result'] = 'Curl failed: ' . curl_error($ch);
-        }
-        else {
+        } else {
             $res['send'] = TRUE;
             $res['result'] = json_decode($result, true);
         }
@@ -199,22 +197,20 @@ class Humanid
 
         return $res;
     }
-    
+
     private function contains($data = NULL, $key = FALSE, $explode = ",")
     {
-        $ststus = FALSE;
-        if(!empty ($data) && $key)
-        {
+        $status = FALSE;
+        if (!empty ($data) && $key) {
             $rows = explode($explode, $data);
-            foreach ($rows as $val)
-            {
-                if($key == $val){
-                    $ststus = TRUE;
+            foreach ($rows as $val) {
+                if ($key == $val) {
+                    $status = TRUE;
                     break;
                 }
             }
         }
 
-        return $ststus;
+        return $status;
     }
 }
