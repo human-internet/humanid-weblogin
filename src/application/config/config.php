b67efff2d57fee0c04f73547d00daa8837c703c8
Saggaf <saggaf.arsyad@gmail.com>
2022-04-22 15:10:28 +0800

    fix(server): Add env to override base url

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -23,10 +23,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = '';
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+// Retrieve base url from env
+$baseUrl = $_ENV['APP_BASE_URL'];
+
+if (!$baseUrl) {
+    $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+    $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+    $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+} else {
+    $config['base_url'] = $baseUrl;
+}
 
 /*
 |--------------------------------------------------------------------------
