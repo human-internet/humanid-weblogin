c00bed65f70893162b421947ea44e11ab6f862b8
raymond <raymond@nusantarabetastudio.com>
2022-04-27 15:22:57 +0700

    feat(recovery existing account): Implement Api for Recovery Existing Account

---
+++ src/application/views/frontend/recovery/change-number-success.php
@@ -0,0 +1,29 @@
+<div class="humanid-modal__overlay active"></div>
+<div class="humanid-modal__modal active" id="modal-agree">
+    <div class="humanid-modal__modal__main recovery-success" style="padding: 40px;">
+        <div class="humanid-logo">
+            <div class="humanid-logo__placement">
+                <img src="<?php echo base_url('images/change-number-success.svg'); ?>"
+                     alt="success change password">
+            </div>
+        </div>
+        <p class="humanid-modal__modal__title">Phone number has been changed</p>
+        <p class="humanid-fw-normal" style="text-align: center; margin-bottom: 60px;">
+            The phone number on your account has been <br>
+            successfully changed and encrypted. You can now login <br>
+            to your previous humanID accounts across all services.
+        </p>
+
+        <div class="humanid-modal__modal__footer">
+            <div class="action-button">
+                <form action="" method="post">
+                    <input type="hidden" name="redirect" value="1">
+                    <button type="submit" class="btn-humanid btn-humanid-primary" data-close="modal-agree"
+                            id="close-popup">
+                        Return to <?php echo $appName ?? ''; ?>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/application/views/frontend/recovery/new-number.php
@@ -0,0 +1,52 @@
+<form action="" method="post">
+    <div class="humanid-logo">
+        <div class="humanid-logo__placement">
+            <img src="<?php echo base_url('assets/images/humanid-logo.svg'); ?>" alt="humanID"
+                 style="">
+        </div>
+    </div>
+
+    <div class="humanid-content-text">
+        <p> humanID is the independent & open online ID - giving back privacy, and the internet, to humans.
+            <br>
+            <a href="javascript:void(0)" class="humanid-link-primary">Learn About Our Mission</a>
+        </p>
+    </div>
+
+    <div class="humanid-page-title">
+        Recover Account
+    </div>
+
+    <div class="humanid-content-text humanid-fz-16 humanid-fw-normal">
+        <p> Let’s transfer your humanID to a new number. First, let’s verify your <b class="humanid-fw-bold">new</b>
+            phone number:
+        </p>
+    </div>
+
+    <div class="humanid-content-text">
+        <div class="humanid-text-info humanid-text-info-danger">
+            <?php if (isset($error_message)): ?><p><?php echo $error_message; ?></p><?php endif; ?>
+        </div>
+    </div>
+
+    <div class="humanid-form-placement">
+        <div class="humanid-form-placement__default">
+            <div class="humanid-form-placement__default-main">
+                <div class="humanid-form-group">
+                    <input type="tel" id="phoneDisplay" class="humanid-input-default" placeholder="812-345-6780"
+                           maxlength="17">
+                    <input type="hidden" name="dialcode" id="dialcode">
+                    <input type="hidden" name="phone" id="phone" value="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="humanid-content-text">
+        OTP verification is managed by an independent 3rd party. Number & fingerprints are never visible to humanID or
+        Green Zone.
+        <a href="javascript:void(0)"><b>Learn More</b></a>
+    </div>
+    <div class="humanid-button humanid-button-vertical" style="display: flex;align-items: center">
+        <button class="btn-humanid btn-humanid-primary" type="submit" style="width: 250px">Login</button>
+    </div>
+</form>
