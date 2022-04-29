81cf3eca5a720d55368884c0c5fd1b6ddf0e9f8a
raymond <raymond@nusantarabetastudio.com>
2022-04-29 12:19:59 +0700

    feat(recovery existing account): Add Link Target On Login Index

--- src/application/views/frontend/login_index.php
+++ src/application/views/frontend/login_index.php
@@ -32,7 +32,6 @@
         <!--<button class="btn-humanid btn-humanid-secondary" type="button"><?php echo $lang->newAccount;?></button>-->
     </div>
     <div>
-        <!-- TODO Change TO Lang function-->
-        <a href="">Got a New Number? Recover Account</a>
+        <a href="<?php echo base_url('recovery/new_number') ?>">Got a New Number? Recover Account</a>
     </div>
 </form>
