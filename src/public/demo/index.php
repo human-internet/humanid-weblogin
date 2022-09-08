15393a68443ebf6168486192ec8111e234cb4048
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-09-08 22:54:36 +0700

    Add page for demo page list
    Took 8 minutes

---
+++ src/public/demo/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HumanID - Demo</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@250&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+    </style>
+    <style>
+        body {
+            background-color: #fff;
+            font: 14px/20px normal Helvetica, Arial, sans-serif;
+            color: #343434;
+            text-align: center;
+        }
+        p {
+            font-family: Lato;
+            font-size: xx-large;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        a:visited {
+            color: #000000;
+        }
+    </style>
+</head>
+<body>
+<p><a href="https://demo1.human-id.org/">Demo 1 (FilmReview)</a></p>
+<p><a href="https://demo2.human-id.org/">Demo 2 (HelloStranger)</a></p>
+</body>
+</html>
