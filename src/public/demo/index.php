fa037c0f9bff23089b4ec92e66113d9504b1a6af
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-09-09 09:11:32 +0700

    Improve demo page
    Took 20 minutes

--- src/public/demo/index.php
+++ src/public/demo/index.php
@@ -1,38 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HumanID - Demo</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@250&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-    </style>
-    <style>
+    <title>The humanID Demo</title>
+    <link rel="icon" href="favicon.png">
+    <style type="text/css">
         body {
             background-color: #fff;
+            margin: 40px;
             font: 14px/20px normal Helvetica, Arial, sans-serif;
-            color: #343434;
+            color: #4F5155;
             text-align: center;
-        }
-        p {
-            font-family: Lato;
-            font-size: xx-large;
         }
 
         a {
+            background: #ffffff;
+            display: inline-block;
+            padding: 10px 100px 10px;
+            border-radius: 10px;
+            border: #000000 solid 2px;
             text-decoration: none;
         }
 
         a:visited {
             color: #000000;
         }
+
+        h1 {
+            line-height: normal;
+        }
     </style>
 </head>
 <body>
-<p><a href="https://demo1.human-id.org/">Demo 1 (FilmReview)</a></p>
-<p><a href="https://demo2.human-id.org/">Demo 2 (HelloStranger)</a></p>
+<h1>Welcome to the humanID demo</h1>
+<img src="//human-id.org/wp-content/uploads/2020/08/humanID-fingerprint.png" alt="Demo App" height="90">
+<br><br><br>
+<p>See how fast & convenient anonymous authorization and taking back control of your data can be</p>
+<div>
+    <a href="https://demo1.human-id.org">Demo 1</a>
+    <a href="https://demo2.human-id.org">Demo 2</a>
+</div>
 </body>
 </html>
