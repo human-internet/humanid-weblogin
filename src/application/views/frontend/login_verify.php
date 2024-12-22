7bf2017fe0945cc81493ad2862f7486674b5524d
Firman Ali Muthohar <firman.ali39@gmail.com>
2024-12-22 09:59:42 +0700

    fix: countdown messy

--- src/application/views/frontend/login_verify.php
+++ src/application/views/frontend/login_verify.php
@@ -2,37 +2,13 @@
 $version = $_GET['v'] ?? 'v1';
 ?>
 <form method="post">
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            let countdown = 60; // Set the countdown starting time in seconds
-            const countdownTime = document.getElementById('countdown-time'); // The <strong> element inside the span
-            const resendLink = document.getElementById('resend-link'); // The resend link
-
-            // Update the countdown every second
-            const timerInterval = setInterval(() => {
-                countdown -= 1; // Decrement the countdown
-
-                if (countdown > 0) {
-                    const minutes = String(Math.floor(countdown / 60)).padStart(2, '0');
-                    const seconds = String(countdown % 60).padStart(2, '0');
-                    countdownTime.textContent = `${minutes}:${seconds}`;
-                } else {
-                    clearInterval(timerInterval); // Stop the countdown
-                    countdownTime.textContent = "00:00"; // Set the countdown to 00:00
-                    resendLink.style.display = 'inline'; // Show the resend link
-                }
-            }, 1000); // Repeat every 1000 milliseconds (1 second)
-        });
-    </script>
-
     <div class="humanid-logo">
         <div class="humanid-logo__placement">
             <img src="<?php echo $app->logoUrls->thumbnail; ?>" alt="<?php echo $app->name; ?>">
         </div>
     </div>
     <?php if ($success): ?>
-        <div
-            class="humanid-page-title"><?php echo str_replace("{APPNAME}", $app->name, $lang->text->pageTitleApp); ?></div>
+        <div class="humanid-page-title"><?php echo str_replace("{APPNAME}", $app->name, $lang->text->pageTitleApp); ?></div>
         <div class="humanid-content-text">
             <div class="humanid-text-info humanid-text-info-default">
                 <p><?php echo $lang->text->welcome; ?></p>
@@ -41,21 +17,23 @@ $version = $_GET['v'] ?? 'v1';
         </div>
         <div class="humanid-button humanid-button-vertical">
             <button class="btn-humanid btn-humanid-primary directed-now"
-                    type="button"><?php echo $lang->redirect; ?></button>
+                type="button"><?php echo $lang->redirect; ?></button>
             <button class="btn-humanid btn-humanid-secondary"
-                    type="button"><?php echo str_replace(array("{TIMER}", "{APPNAME}"), array('<span class="timer-text"></span>', $app->name), $lang->text->timer); ?></button>
+                type="button"><?php echo str_replace(array("{TIMER}", "{APPNAME}"), array('<span class="timer-text"></span>', $app->name), $lang->text->timer); ?></button>
             <input type="hidden" class="directed-link" value="<?php echo $redirectUrl; ?>">
         </div>
         <?php /*if(!$hasSetupRecovery && $accountRecovery === true) { */ ?><!--
                 <div class="humanid-content-link center">
                     <a href="<?php /*echo $redirectSetRecoveryEmail;*/ ?>" class="humanid-link-red">Recover an existing account instead</a>
                 </div>
-        --><?php /*} */ ?>
+        -->
+        <?php /*} */ ?>
     <?php else: ?>
         <div class="humanid-page-title"><?php echo $lang->verify; ?></div>
         <div class="humanid-content-text">
             <div class="humanid-text-info humanid-text-info-default">
-                <p><?php echo str_replace(array("{COUNTRYCODE}", "{PHONE}"), array($row['dialcode'], $display_phone), $lang->text->verifyCode); ?></p>
+                <p><?php echo str_replace(array("{COUNTRYCODE}", "{PHONE}"), array($row['dialcode'], $display_phone), $lang->text->verifyCode); ?>
+                </p>
                 <p><?php echo $lang->text->afterSuccessful; ?></p>
             </div>
             <div class="humanid-text-info humanid-text-info-danger">
@@ -67,19 +45,18 @@ $version = $_GET['v'] ?? 'v1';
             <div class="humanid-form-placement__otp-verification">
                 <?php if (isset($_GET['s']) && $_GET['s'] == 'm') { ?>
                     <div class="humanid-form-group" style="flex: 3">
-                        <input type="tel" oninput="humanid.handleInputOtp(this, true)"
-                               maxlength="4" pattern="[0-9]*" class="humanid-input-otp" name="code"
-                               id="single-factor-code-text-field" autocomplete="one-time-code">
+                        <input type="tel" oninput="humanid.handleInputOtp(this, true)" maxlength="4" pattern="[0-9]*"
+                            class="humanid-input-otp" name="code" id="single-factor-code-text-field"
+                            autocomplete="one-time-code">
                     </div>
-                    <button class="btn-humanid btn-humanid-primary btn-submit-otp" disabled type="submit"
-                            style="flex: 1">Submit
+                    <button class="btn-humanid btn-humanid-primary btn-submit-otp" disabled type="submit" style="flex: 1">Submit
                     </button>
                 <?php } else { ?>
                     <div class="humanid-form-group">
                         <input type="tel" oninput="humanid.handleInputOtp(this, false)"
-                               onpaste="humanid.handlePasteOtp(this, event)"
-                               maxlength="4" pattern="[0-9]*" class="humanid-input-otp" name="code"
-                               id="single-factor-code-text-field" autocomplete="one-time-code">
+                            onpaste="humanid.handlePasteOtp(this, event)" maxlength="4" pattern="[0-9]*"
+                            class="humanid-input-otp" name="code" id="single-factor-code-text-field"
+                            autocomplete="one-time-code">
                     </div>
                 <?php } ?>
             </div>
@@ -89,12 +66,12 @@ $version = $_GET['v'] ?? 'v1';
             <div class="humanid-form-placement__link">
                 <div class="humanid-form-placement__link__wrapper">
                     <a href="<?php echo site_url('login?a=' . $app->id . '&t=' . $row['token'] . '&lang=' . $lang->id . '&priority_country=' . $pc->code . '&s=' . $app->source . '&v=' . $version); ?>"
-                       class="humanid-link-blue-light">
+                        class="humanid-link-blue-light">
                         <?php echo $lang->try; ?>
                     </a>
                     <a href="<?php echo site_url('login/resend?a=' . $app->id . '&t=' . $row['token'] . '&lang=' . $lang->id . '&s=' . $app->source . '&v=' . $version); ?>"
-                       id="resend-link" class="humanid-link-blue-light"
-                       style="display: none;"><?php echo $lang->resend; ?></a>
+                        id="resend-link" class="resend-area humanid-link-blue-light"
+                        style="display: none;"><?php echo $lang->resend; ?></a>
                     <!--<a href="<?php /*echo base_url('recovery/new_number') */ ?>" class="humanid-link-blue-light">Recover Existing Account</a>-->
                 </div>
             </div>
